42ff6f8b5bec9395c3e28da89a0c116544c4b793
João Peixoto - Work <joaottvitor04@gmail.com>
2025-09-01 13:36:46 -0300

    feat: Implementar suporte a envio de e-mails com SMTP e NoopMailSender

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,13 @@ import br.com.mariojp.solid.dip.*;
 public class Main {
 	public static void main(String[] args) {
 		System.setProperty("DRY_RUN", "true"); // desejável não chamar SMTP
-		var notifier = new EmailNotifier();
+		MailSender sender;
+		if ("true".equalsIgnoreCase(System.getProperty("DRY_RUN"))) {
+			sender = new NoopMailSender();
+		} else {
+			sender = new SmtpMailSender(new SmtpClient());
+		}
+		var notifier = new EmailNotifier(sender);
 		// Estado inicial: vai lançar IllegalStateException (SMTP indisponível)
 		notifier.welcome(new User("Ana", "ana@example.com"));
 		System.out.println("Email enviado!");
--- src/main/java/br/com/mariojp/solid/dip/EmailNotifier.java
+++ src/main/java/br/com/mariojp/solid/dip/EmailNotifier.java
@@ -1,12 +1,13 @@
 package br.com.mariojp.solid.dip;
 
 public class EmailNotifier {
-	private final SmtpClient smtp = new SmtpClient();
+	private final MailSender smtp;
 
-	public void welcome(User user) {
-		if ("true".equalsIgnoreCase(System.getProperty("DRY_RUN"))) {
-			// Estado inicial: ainda assim usa SMTP real (bug proposital)
-		}
+	public EmailNotifier(MailSender mailSender) {
+		this.smtp = mailSender;
+	}
+
+	public void welcome(User user) {	
 		smtp.send(user.email(), "Bem-vindo", "Olá " + user.name());
 	}
 }
